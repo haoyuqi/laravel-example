83fd9f7b647b0ae5d287f48f37d6023218b8ee69
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2019-09-21 15:10:34 +0800

    路由优化

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    public function index()
+    {
+        return view('welcome');
+    }
+
     public function test()
     {
         abort(403);
