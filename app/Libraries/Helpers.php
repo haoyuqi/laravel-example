3e379229759fcf745423bb428ef105b0627a23ce
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2019-11-03 09:20:38 +0800

    新增 get_real_ip 函数

--- app/Libraries/Helpers.php
+++ app/Libraries/Helpers.php
@@ -4,3 +4,10 @@
  * 辅助函数
  * Date: 2019/11/3
  */
+
+if (!function_exists('get_real_ip')) {
+    function get_real_ip()
+    {
+        return request()->hasHeader('X-Real-IP') ? request()->header('X-Real-IP') : request()->getClientIp();
+    }
+}
