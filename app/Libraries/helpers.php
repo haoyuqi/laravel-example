9f1ac6a08e9820d24eb4192c508f753a60ad342c
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2019-11-05 21:39:45 +0800

    新增 bubble_sort function

--- app/Libraries/helpers.php
+++ app/Libraries/helpers.php
@@ -11,3 +11,26 @@ if (!function_exists('get_real_ip')) {
         return request()->hasHeader('X-Real-IP') ? request()->header('X-Real-IP') : request()->getClientIp();
     }
 }
+
+if (!function_exists('bubble_sort')) {
+    function bubble_sort(array $arr)
+    {
+        $count = count($arr);
+
+        if ($count < 2) {
+            return $arr;
+        }
+
+        for ($i = 0; $i < $count; $i++) {
+            for ($j = $i + 1; $j < $count; $j++) {
+                if ($arr[$i] > $arr[$j]) {
+                    $temp = $arr[$j];
+                    $arr[$j] = $arr[$i];
+                    $arr[$i] = $temp;
+                }
+            }
+        }
+
+        return $arr;
+    }
+}
