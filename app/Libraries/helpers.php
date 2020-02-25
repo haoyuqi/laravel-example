b6e34210d45dec14561f45492edf32ab41936305
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-02-25 20:59:29 +0800

    安装 sort-functions

--- app/Libraries/helpers.php
+++ app/Libraries/helpers.php
@@ -17,34 +17,6 @@ if (!function_exists('get_real_ip')) {
     }
 }
 
-if (!function_exists('bubble_sort')) {
-    /**
-     * 冒泡排序
-     * @param array $arr
-     * @return array
-     */
-    function bubble_sort(array $arr)
-    {
-        $count = count($arr);
-
-        if ($count < 2) {
-            return $arr;
-        }
-
-        for ($i = 0; $i < $count; $i++) {
-            for ($j = $i + 1; $j < $count; $j++) {
-                if ($arr[$i] > $arr[$j]) {
-                    $temp = $arr[$j];
-                    $arr[$j] = $arr[$i];
-                    $arr[$i] = $temp;
-                }
-            }
-        }
-
-        return $arr;
-    }
-}
-
 if (!function_exists('quick_sort')) {
     /**
      * 快速排序
