b7ae5cccb42ec597436e4fcd9f05c500bfb3aefc
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-05-03 10:45:33 +0800

    修改 admin home 页面

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -14,9 +14,8 @@ class HomeController extends Controller
     {
         return $content
             ->title('Dashboard')
-            ->description('Description...')
+            ->row(view('admin.title'))
             ->row(function (Row $row) {
-
                 $row->column(4, function (Column $column) {
                     $column->append(Dashboard::environment());
                 });
