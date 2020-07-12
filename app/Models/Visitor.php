95e12a558d077c454245c836a7128581c535fc6d
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-07-12 08:36:46 +0800

    升级 laravel

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Visitor extends Model
+class Visitor extends BaseModel
 {
     use SoftDeletes;
 
--- app/Models/VisitorLog.php
+++ app/Models/VisitorLog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class VisitorLog extends Model
+class VisitorLog extends BaseModel
 {
     use SoftDeletes;
 
