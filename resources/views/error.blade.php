d1bfff4ce0e50452b512c27dd4b9b39af0c31afe
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-07-24 21:49:04 +0800

    修改页面

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.index')
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('title', 'Error')
 
-    <title>laravel example</title>
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-</head>
-<body>
-<div id="app">
-    @if($errors->any())
-        <show-info info="{{ $errors->first() }}"></show-info>
-    @else
-        <show-info info="{{ $info }}"></show-info>
-    @endif
-</div>
-<script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@section('content')
+    <div id="app">
+        @if($errors->any())
+            <show-info info="{{ $errors->first() }}"></show-info>
+        @else
+            <show-info info="{{ $info }}"></show-info>
+        @endif
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.index')
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('title', 'Laravel example')
 
-    <title>laravel example</title>
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-</head>
-<body>
-{{--@if (Route::has('login'))
+@section('content')
+    {{--@if (Route::has('login'))
     <div class="top-right links">
         @auth
             <a href="{{ url('/home') }}">Home</a>
@@ -25,9 +16,8 @@
         @endauth
     </div>
 @endif--}}
-<div id="app">
-    <show-info info="{{ $info }}"></show-info>
-</div>
-<script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+    <div id="app">
+        <show-info info="{{ $info }}"></show-info>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+@endsection
