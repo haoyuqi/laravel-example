c0eda9819216bbe084970b7369fb5cb7d375c85b
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2021-03-25 22:08:50 +0800

    replace cypress with laravel dusk

--- tests/cypress/integration/case/Web/Home.feature
+++
@@ -1,5 +0,0 @@
-Feature: Home
-
-    Scenario: I want to open the homepage
-        Given Open web site
-        Then See text
--- tests/cypress/integration/case/Web/Home/home.js
+++
@@ -1,9 +0,0 @@
-import {Given, Then} from "cypress-cucumber-preprocessor/steps";
-
-Given('Open web site', () => {
-    cy.visit(Cypress.env('HOST'));
-});
-
-Then('See text', () => {
-    cy.contains('Hello World').should('be.visible');
-});
