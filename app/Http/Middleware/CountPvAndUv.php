0996087fe27faec97fcc000bf7b4b26baaee68fd
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2021-12-04 11:00:53 +0800

    visitor statistics

---
+++ app/Http/Middleware/CountPvAndUv.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class CountPvAndUv
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $ip = $request->getClientIp();
+
+        $uv_key = 'uv_set_' . now()->toDateString();
+        Redis::sadd($uv_key, $ip);
+
+        $pv_key = 'pv_count_' . now()->toDateString();
+        Redis::incr($pv_key);
+
+        return $next($request);
+    }
+}
